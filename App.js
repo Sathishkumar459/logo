c75313cb8e82a29d5e53fb6a9a5adc9ecb8391e1
Sathishkumar459 <114897643+Sathishkumar459@users.noreply.github.com>
2022-12-04 13:29:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Design from './Components/Design.js';
+import './Styles/color.css'
+
+function App() {
+  return (
+    <div className="App">
+     <Design/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Design.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+class Design extends React.Component{
+    render(){
+        return(
+            <div>
+                <div className="container">
+                    
+                   <div className="divName ">
+                    
+                   <div className="box1 ">
+                   <div className="box2 ">
+                   <div className="box3 ">
+                   <div className="box4 ">
+                   <div className="text">HTML<span>&</span>CSS<br></br>design and build website</div>
+                    
+                    </div>
+                    </div></div>
+                   </div>
+                   </div> 
+                   
+                
+                </div>       
+            </div>
+        )
+    }
+}
+export default Design;
